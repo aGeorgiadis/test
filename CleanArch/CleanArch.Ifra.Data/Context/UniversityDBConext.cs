9153ebce46a6695da3d46c7a0c31f005245b33f9
Angelos Georgiadis <angelos.georgiadis@gr.ey.com>
2023-02-17 10:51:34 +0200

    nu packages installation

---
+++ CleanArch/CleanArch.Ifra.Data/Context/UniversityDBConext.cs
@@ -0,0 +1,19 @@
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Ifra.Data.Context
+{
+    public   class UniversityDBConext:DbContext
+    {
+        public UniversityDBConext(DbContextOptions options):base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
